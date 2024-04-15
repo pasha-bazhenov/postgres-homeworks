d35094a2aeb437ac4192d4cd3304fd1cb886fbb4
Pasha <pab.02@bk.ru>
2024-04-15 13:22:54 +0300

    upgrade create_tables.sql

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,23 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(employee_id serial,
+	first_name varchar (100) NOT NULL,
+	last_name varchar (100) NOT NULL,
+	title varchar (100) NOT NULL,
+	birth_date date ,
+	notes text
+);
+
+CREATE TABLE customers
+(customer_id char (5) NOT NULL,
+ company_name varchar (100) NOT NULL,
+ contact_name varchar (100) NOT NULL
+);
+
+CREATE TABLE orders
+(order_id int,
+ customer_id char (5) NOT NULL,
+ employee_id serial,
+ order_date date,
+ ship_city text
+);
